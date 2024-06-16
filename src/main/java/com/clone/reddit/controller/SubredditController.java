9ea5fd351ef90292c9f0a4487d69d5b93182422f
thony <dayondonthony@gmail.com>
2024-06-17 02:14:34 +0800

    implement spring security for fine grain access control

--- src/main/java/com/clone/reddit/controller/SubredditController.java
+++ src/main/java/com/clone/reddit/controller/SubredditController.java
@@ -1,24 +1,20 @@
 package com.clone.reddit.controller;
 
 import com.clone.reddit.model.Response;
-import com.clone.reddit.model.UserAccount;
 import com.clone.reddit.service.implementation.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 import static java.time.LocalDateTime.now;
-import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequiredArgsConstructor
-public class RegisterController {
+public class SubredditController {
 
     private final UserServiceImpl userService;
 
@@ -39,20 +35,5 @@ public class RegisterController {
                         .build()
         );
     }
-
-    @PostMapping("/api/register/save")
-    public ResponseEntity<Response> saveServer(@RequestBody UserAccount user)
-    {
-        return ResponseEntity.ok(
-            Response.builder()
-                    .timeStamp(now())
-                    .data(Map.of("server", userService.create(user)))
-                    .message("User created")
-                    .status(CREATED)
-                    .statusCode(CREATED.value())
-                    .build()
-        );
-    }
-
 
 }
--- src/main/java/com/clone/reddit/service/UserService.java
+++ src/main/java/com/clone/reddit/service/UserService.java
@@ -1,10 +1,11 @@
 package com.clone.reddit.service;
 
+import com.clone.reddit.model.AuthenticationResponse;
 import com.clone.reddit.model.UserAccount;
 
 import java.util.Collection;
 
 public interface UserService {
-    UserAccount create(UserAccount user);
+    AuthenticationResponse register(UserAccount user);
     Collection<UserAccount> list();
 }
